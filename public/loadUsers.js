aa2ab308d4abb1fec16064e5f2c2aa5c68be0419
Armando Velazquez <davidarmando.gdl@live.com>
2020-02-22 01:29:45 -0600

    data rendered and filter working

--- public/loadUsers.js
+++ public/loadUsers.js
@@ -1,11 +1,28 @@
 async function getUsers() {
   const users = await fetch("http://localhost:3000/api/users");
   const solvedUsers = await users.json();
-  const result = solvedUsers.data.map(user => {
-    const { username, phone, email } = user;
-    return `<p>${username} <strong>${email}</strong></p><p>${phone}</p>`;
+  const result = solvedUsers.map(user => {
+    const { username, phone, email, address } = user;
+    return `<article class='user__card'><p class='user__card-username'>${username}</p><p class='user__card-email'>${email}</p><p class='user__card-phone'>${phone}</p><p class='user__card-city'>${address.city}</p></article>`;
   });
-  document.body.innerHTML = result.join("");
+  document.getElementById("root").innerHTML = result.join("");
 }
 
-getUsers();
+async function filterUsers(filter) {
+  const users = await fetch(`http://localhost:3000/api/users/${filter}`);
+  const solvedUsers = await users.json();
+  const result = solvedUsers.map(user => {
+    const { username, phone, email, address } = user;
+    return `<article class='user__card'><p class='user__card-username'>${username}</p><p class='user__card-email'>${email}</p><p class='user__card-phone'>${phone}</p><p class='user__card-city'>${address.city}</p></article>`;
+  });
+  document.getElementById("root").innerHTML = result.join("");
+}
+
+window.onload = () => {
+  getUsers();
+
+  document.getElementById("input__filter").addEventListener("keyup", () => {
+    const myFilter = document.getElementById("input__filter").value;
+    filterUsers(myFilter);
+  });
+};
--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,17 @@ module.exports = app => {
   router.get("/users", async (req, res) => {
     const users = await Fetch("https://jsonplaceholder.typicode.com/users");
     const solvdedUseres = await users.json();
-    res.status(200).json({
-      data: solvdedUseres,
-      message: "Users listed"
+    res.status(200).json(solvdedUseres);
+  });
+
+  router.get("/users/:user", async (req, res) => {
+    const { user } = req.params;
+    const users = await Fetch("https://jsonplaceholder.typicode.com/users");
+    const solvedUsers = await users.json();
+    const filteredUsers = solvedUsers.filter(cUser => {
+      const filterRegex = new RegExp(user, "gi");
+      return filterRegex.test(cUser.username);
     });
+    res.json(filteredUsers);
   });
 };
