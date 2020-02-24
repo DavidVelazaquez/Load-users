2c6672eaf192b4cafde91be1e5bbe1a9e5f459c3
Armando Velazquez <davidarmando.gdl@live.com>
2020-02-24 00:35:57 -0600

    data is filtered

--- routes/api.js
+++ routes/api.js
@@ -6,19 +6,27 @@ module.exports = app => {
   app.use("/api", router);
 
   router.get("/users", async (req, res) => {
-    const users = await Fetch("https://jsonplaceholder.typicode.com/users");
-    const solvdedUseres = await users.json();
-    res.status(200).json(solvdedUseres);
+    // const users = await Fetch("https://jsonplaceholder.typicode.com/users");
+    // const solvdedUseres = await users.json();
+    // const solvedUsers = await getUsers();
+    res.status(200).json(await getUsers());
   });
 
   router.get("/users/:user", async (req, res) => {
     const { user } = req.params;
-    const users = await Fetch("https://jsonplaceholder.typicode.com/users");
-    const solvedUsers = await users.json();
+    // const users = await Fetch("https://jsonplaceholder.typicode.com/users");
+    // const solvedUsers = await users.json();
+    const solvedUsers = await getUsers();
     const filteredUsers = solvedUsers.filter(cUser => {
       const filterRegex = new RegExp(user, "gi");
       return filterRegex.test(cUser.username);
     });
     res.json(filteredUsers);
   });
+};
+
+const getUsers = async () => {
+  const users = await Fetch("https://jsonplaceholder.typicode.com/users");
+  const solvedUsers = await users.json();
+  return solvedUsers;
 };
